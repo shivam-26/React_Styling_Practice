341ce866bd85293dedac72e6fc4f56f97107a5c4
shivam-26 <79416882+shivam-26@users.noreply.github.com>
2021-12-30 12:47:56 +0530

    React Styling practice

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const time = new Date();
+const currentTime = time.getHours();
+//console.log(currentTime);
+
+const customStyle = {
+  color: ""
+};
+let greeting;
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "Red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "Blue";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "Green";
+}
+
+ReactDOM.render(
+  <h1 className="heading" style={customStyle}>
+    {" "}
+    {greeting}{" "}
+  </h1>,
+  document.getElementById("root")
+);
+
 //Create a React app from scratch.
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
 //or "Good Afternoon" if between 12PM and 6PM.
